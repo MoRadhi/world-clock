b13b6ca8f98fd3212bd283977c3abc35af3a8fc5
moradhi <mohammedabdulla05@hotmail.com>
2026-02-07 17:29:38 +0300

    Adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,28 @@ import "./App.css";
 import WorldClock from "./Components/WorldClock";
 
 function App() {
-  const data = [
-      "America/Los_Angeles",
-      "Europe/London",
-      "Asia/Singapore"
-  ]
+  const data = ["America/Los_Angeles", "Europe/London", "Asia/Singapore"];
 
-    return (
+  return (
     <>
-        <h1>World Clock</h1>
+      <h1>World Clock</h1>
 
       <div>
-        <img src="../public/Map.jpg" alt="Map Image"  style={{
+        <img
+          src="../public/Map.jpg"
+          alt="Map Image"
+          style={{
             display: "block",
             margin: "0 auto",
-            width: "700px",  // Adjust this value to make it smaller
-            height: "auto"
-        }} />
+            width: "700px",
+            height: "auto",
+          }}
+        />
       </div>
 
-        <div className="App">
-            <WorldClock
-                clockData={data}
-            />
-        </div>
+      <div className="App">
+        <WorldClock clockData={data} />
+      </div>
     </>
   );
 }
